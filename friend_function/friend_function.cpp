b389c56ba78791747c473af66760964457616349
Shakhin Nabiev <shakhin@mail.ru>
2024-04-01 08:58:01 +0300

    Добавьте файлы проекта.

---
+++ friend_function/friend_function.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+class Number {
+private:
+    int a;
+public:
+    Number (int a) {
+    this->a = a;
+}
+    friend int printNum(Number NUM)
+    {
+        return NUM.a + NUM.a;
+        
+    }
+};
+
+int main()
+{
+    Number n(1000);
+    cout<<printNum(n)<<endl;
+
+    return 0;
+}
